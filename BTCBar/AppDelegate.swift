f1e391a84a5b092a733e4b724a5c7c27fbee506a
Tejas Prakash <tejasprak@gmail.com>
2017-01-23 14:11:48 -0800

    Initial Commit

---
+++ BTCBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BTCBar
+//
+//  Created by Tejas Prakash on 1/23/17.
+//  Copyright © 2017 tejasp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
